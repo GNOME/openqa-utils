14bf64b95be597814642e4c5f64c356528f4e73f
Sam Thursfield <sam.thursfield@codethink.co.uk>
2025-03-11 23:19:41 +0100

    Set to 100 since 0 doesn't appear to work as described.
    The openQA `workers.ini` template says this:
    
        # Set to 0 to disable.
    
    This doesn't appear to work, at least as seen here where the jobs
    still did not start:
    <https://gitlab.gnome.org/GNOME/openqa-tests/-/jobs/4856680>.

--- setup_worker.sh
+++ setup_worker.sh
@@ -19,5 +19,5 @@ HOST = $OPENQA_HOST
 WORKER_HOSTNAME = $(hostname).no-route.example.com
 # This limit isn't helpful due to the unusual way GNOME's openQA provides workers.
 # See <https://gitlab.gnome.org/GNOME/openqa-tests/-/issues/126>
-CRITICAL_LOAD_AVG_THRESHOLD = 0
+CRITICAL_LOAD_AVG_THRESHOLD = 100
 EOF
